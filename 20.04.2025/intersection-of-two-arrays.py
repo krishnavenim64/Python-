efde3bd3f35bad763f28f9be0f296c952871eb85
krishnavenim64 <krishnavenim122@gmail.com>
2025-04-28 22:45:27 +0530

    Create intersection-of-two-arrays.py

---
+++ 20.04.2025/intersection-of-two-arrays.py
@@ -0,0 +1,13 @@
+Class Solution:
+
+    Def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        Set1 = set(nums1)
+
+        Set2 = set(nums2)
+
+
+
+        # Find the intersection of the two sets
+
+        Return list(set1 & set2)
