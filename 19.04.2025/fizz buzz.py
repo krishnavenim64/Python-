0c60a876a33d6dc4fdf64f64dd5e20bbb1f5b034
krishnavenim64 <krishnavenim122@gmail.com>
2025-04-28 22:36:53 +0530

    Create fizz buzz.py

---
+++ 19.04.2025/fizz buzz.py
@@ -0,0 +1,12 @@
+Class Solution(object):
+
+    Def fizzBuzz(self, n):
+
+        Return [
+
+            “FizzBuzz” if i % 15 == 0 else “Fizz” if i % 3 == 0 else “Buzz” if i % 5 == 0 else str(i)
+
+            For i in range(1, n + 1)
+
+        ]
+
