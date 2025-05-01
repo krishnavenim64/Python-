faa23ad064517405a5646482b19d82cfa1d67685
krishnavenim64 <krishnavenim122@gmail.com>
2025-05-01 16:08:02 +0530

    Create pow(x,n).py

---
+++ 25.04.2025/pow(x,n).py
@@ -0,0 +1,16 @@
+Class Solution(object):
+
+    Def myPow(self, x, n):
+
+        “””
+
+        :type x: float
+
+        :type n: int
+
+        :rtype: float
+
+        “””
+
+        Return x ** n
+
