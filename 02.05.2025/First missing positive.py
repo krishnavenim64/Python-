bcc2c739e06bcedf221fd41e94adcd367154ba0f
krishnavenim64 <krishnavenim122@gmail.com>
2025-05-03 18:38:23 +0530

    Create First missing positive.py

---
+++ 02.05.2025/First missing positive.py
@@ -0,0 +1,16 @@
+Class Solution(object):
+
+    Def firstMissingPositive(self, nums):
+
+      Num = 1
+
+      Nums = set(nums)
+
+      While True:
+
+         If num not in nums:
+
+            Return num
+
+         Num += 1
+
