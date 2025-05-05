04d65be74a883f8f6543f792498c7c49c9137050
krishnavenim64 <krishnavenim122@gmail.com>
2025-05-05 11:34:23 +0530

    Create delete columns to make sorted.py

---
+++ 07.05.2025/delete columns to make sorted.py
@@ -0,0 +1,28 @@
+Class Solution(object):
+
+    Def minDeletionSize(self, strs):
+
+        A=len(strs[0])
+
+        K=0
+
+        L=[]
+
+        For x in range(a):
+
+            For y in strs:
+
+                l.append(y[x])
+
+            e=l[:]
+
+            e.sort()
+
+            if e!=l:
+
+                k=k+1
+
+            l=[]
+
+        return k
+
