1817feb41e15fd0ce520871fc875c563f84c9bd4
krishnavenim64 <krishnavenim122@gmail.com>
2025-05-05 11:33:50 +0530

    Create DI string match.py

---
+++ 07.05.2025/DI string match.py
@@ -0,0 +1,34 @@
+Class Solution(object):
+
+    Def diStringMatch(self, s):
+
+        Ans = []
+
+
+
+        I = 0
+
+        J = len(s)
+
+
+
+        For c in s :
+
+            If c == “I”:
+
+                Ans.append(i)
+
+                I += 1
+
+            Elif c == “D”:
+
+                Ans.append(j)
+
+                J -= 1
+
+
+
+        Ans.append(j)
+
+        Return ans
+
